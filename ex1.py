1dd372714267ecff604d5d72ad9ed8c4cda4998d
vsai121 <vsaicharan1998@gmail.com>
2018-01-08 19:07:50 +0530

    Policy Evaluation

--- ex1.py
+++ ex1.py
@@ -1,12 +1,21 @@
 import gym
 env = gym.make('CartPole-v0')
+
+print(env.action_space)
+print(env.observation_space)
+
+action1 = env.action_space.sample()
+print(action1)    
+    
 for i_episode in range(20):
     observation = env.reset()
     for t in range(100):
         env.render()
-        print(observation)
+        #print(observation)
         action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
+        observation, reward, done, info = env.step(action1)
+        #print("Reward: "),
+        #print(reward)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
